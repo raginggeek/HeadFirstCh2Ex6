ba1c10f8d9f75f8be3c087eeb37de8e7b456e24e
Geoff Finch <gfinch1@gmail.com>
2017-07-02 16:23:52 -0500

    finished last Chapter 2 Exercise

--- HeadFirstCh2Ex6/Form1.cs
+++ HeadFirstCh2Ex6/Form1.cs
@@ -16,5 +16,24 @@ namespace HeadFirstCh2Ex6
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            while (Visible)
+            {
+                for (int c = 0; c < 253 && Visible; c++)
+                {
+                    this.BackColor = Color.FromArgb(c, 255 - c, c);
+                    Application.DoEvents();
+                    System.Threading.Thread.Sleep(3);
+                }
+                for (int c = 254; c > 0 && Visible; c--)
+                {
+                    this.BackColor = Color.FromArgb(c, 255 - c, c);
+                    Application.DoEvents();
+                    System.Threading.Thread.Sleep(3);
+                }
+            }
+        }
     }
 }
